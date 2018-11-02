af35744e54563498e4df00cc2a66f0aeb3e13c0a
Malte Bublitz <malte70@tuta.io>
2018-11-02 20:41:37 +0100

    [zsh] Added l1 alias for ls -1 (one file per line mode)

--- .zsh/alias.zsh
+++ .zsh/alias.zsh
@@ -42,6 +42,7 @@ elif which pacapt &>/dev/null; then
 fi
 if [[ $OS != "Mac OS X" && $OS != "FreeBSD" ]]; then
 	alias ls="`print -n =ls` --color=auto --escape -l --file-type -h --time-style=long-iso"
+	alias l1="`print -n =ls` --color=auto --escape -1"
 	alias du="`print -n =du` --summarize --total --human-readable"
 	alias d=`print -n =date`' --rfc-3339=seconds | tr " " "T"'
 	if [[ $OS != "Android" ]]; then
@@ -55,12 +56,14 @@ else
 	if which gls &>/dev/null; then
 		# If gls is available, all other coreutils should be too.
 		alias ls="`print -n =gls` --color=auto --escape -l --file-type -h --time-style=long-iso"
+		alias l1="`print -n =gls` --color=auto --escape -1"
 		alias df="`print -n =gdf` --human-readable --print-type"
 		alias du="`print -n =gdu` --summarize --total --human-readable"
 		alias d=`print -n =gdate`' --rfc-3339=seconds | tr " " "T"'
 		alias sed="`print -n =gsed`"
 	else
 		alias ls="/bin/ls -l -G -F -b -h"
+		alias l1="/bin/ls -1"
 		alias df="/bin/df -h"
 		alias d='date "+%Y-%m-%dT%H:%M:%S%z"'
 	fi
