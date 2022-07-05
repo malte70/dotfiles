7aa48501d00c67d19df4b168156bb007fda79eb4
Malte Bublitz <malte@rolltreppe3.de>
2022-07-05 15:14:41 +0200

    [zsh] Fixed bat alias to work after zshrc-reload

--- .zsh/alias.zsh
+++ .zsh/alias.zsh
@@ -346,8 +346,7 @@ if which exa &>/dev/null; then
 	alias tree="$(which exa) --long --header -T --icons"
 fi
 if which bat &>/dev/null; then
-	#alias bat="/usr/bin/bat --plain -P"
-	alias bat="$(which bat) --paging=never"
+	alias bat="`print -n =bat` --paging=never"
 	alias cat="bat"
 	alias ccat="bat"
 fi
