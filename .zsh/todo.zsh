e785f16905e7b307ad909d69feb43a50ec2dfc63
Malte Bublitz <malte@rolltreppe3.de>
2023-09-19 22:03:34 +0200

    [zsh] ✨ Added config option to disable notes

--- .zsh/todo.zsh
+++ .zsh/todo.zsh
@@ -45,7 +45,7 @@ then
 fi
 
 # Show notes when shell starts (but not if .zshrc is sourced again!)
-if [[ "$DID_SHOW_NOTES" != "yes" && -f "$NOTES" ]]
+if [[ "$SHOW_NOTES" == "yes" && "$DID_SHOW_NOTES" != "yes" && -f "$NOTES" ]]
 then
 	# Only show if 10 lines or less to not mess up the screen
 	if [[ $(wc -l "$NOTES" | cut -d" " -f1) -lt 12 ]]
